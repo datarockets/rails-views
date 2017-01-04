0a3c7f94f634519c8ba01e53ec4adc181e76deb1
Roman Dubrovsky <roma2107946@gmail.com>
2017-01-04 12:11:55 +0300

    Start commit

---
+++ lib/cells/helpers.rb
@@ -0,0 +1,7 @@
+require "cells/helpers/version"
+
+module Cells
+  module Helpers
+    # Your code goes here...
+  end
+end
---
+++ lib/cells/helpers/version.rb
@@ -0,0 +1,5 @@
+module Cells
+  module Helpers
+    VERSION = "0.1.0"
+  end
+end
