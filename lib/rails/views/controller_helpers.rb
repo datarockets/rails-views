b2dd7174be8a138a342c21a5b8d0c2318949c194
Roman Dubrovsky <roma2107946@gmail.com>
2017-01-12 12:36:36 +0300

    Small refactoring

--- lib/rails/views/controller_helpers.rb
+++ lib/rails/views/controller_helpers.rb
@@ -8,26 +8,18 @@ module Rails
 
         base.class_eval do
           alias_method_chain :render, :cells
-          # alias_method_chain :render_to_string, :cells
         end
       end
 
       def self.prepended(base)
-        # Protect from trying to augment modules that appear
-        # as the result of adding other gems.
         return if base != ActionController::Base
 
         base.class_eval do
           alias_method :render_without_cells, :render
-          # alias_method :render_to_string_without_cells, :render_to_string
 
           def render(options = nil, *args, &block)
             render_with_cells(options, *args, &block)
           end
-
-          # def render_to_string(options = nil, *args, &block)
-          #   render_to_string_with_cells(options, *args, &block)
-          # end
         end
       end
 
